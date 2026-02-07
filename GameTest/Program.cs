0118fdd01a9d57863ac05f7d778f89bf9d9689a8
François Ségaud <francois@fseg.io>
2026-02-07 14:34:58 +0100

    VirtualScreen colors
    Added colors to VirtualScreen
    Added enemy yo GameTest.

--- GameTest/Program.cs
+++ GameTest/Program.cs
@@ -28,11 +28,27 @@ public static class Program
             processor.Run();
             if (processor.IsFinished)
                 break;
-            
+
+            ColorSet colorSet = default;
             for (var y = 0; y < screen?.Height; y++)
             {
                 for (var x = 0; x < screen.Width; x++)
-                    Console.Write(screen.Data[y * screen.Width + x]);
+                {
+                    int index = y * screen.Width + x;
+                    if (screen.Color[index] != 0)
+                    {
+                        GetColor(screen.Color[index], ref colorSet);
+                        Console.BackgroundColor = colorSet.Background;
+                        Console.ForegroundColor = colorSet.Foreground;
+                        Console.Write(screen.Data[index]);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write(screen.Data[index]);
+                    }
+                }
+
                 Console.WriteLine();
             }
 
@@ -45,5 +61,46 @@ public static class Program
         {
             ConsoleHelper.PrintFailedTest(SrcFile, processor.LastError, "Runtime");
         }
+    }
+
+    private static void GetColor(byte consoleColorByte, ref ColorSet colorSet)
+    {
+        colorSet.Background = (consoleColorByte >> 4) switch
+        {
+            0x0 => ConsoleColor.Black,
+            0x1 => ConsoleColor.Red,
+            0x2 => ConsoleColor.Green,
+            0x3 => ConsoleColor.Blue,
+            0x4 => ConsoleColor.Cyan,
+            0x5 => ConsoleColor.Magenta,
+            0x6 => ConsoleColor.Yellow,
+            0xa => ConsoleColor.Black,
+            0xb => ConsoleColor.White,
+            0xc => ConsoleColor.Gray,
+            0xd => ConsoleColor.DarkGray,
+        };
+        
+        colorSet.Foreground = (consoleColorByte & 0x0f) switch
+        {
+            0x0 => ConsoleColor.White,
+            0x1 => ConsoleColor.Red,
+            0x2 => ConsoleColor.Green,
+            0x3 => ConsoleColor.Blue,
+            0x4 => ConsoleColor.Cyan,
+            0x5 => ConsoleColor.Magenta,
+            0x6 => ConsoleColor.Yellow,
+            0xa => ConsoleColor.Black,
+            0xb => ConsoleColor.White,
+            0xc => ConsoleColor.Gray,
+            0xd => ConsoleColor.DarkGray,
+            
+            _ => throw new ArgumentOutOfRangeException(nameof(consoleColorByte), consoleColorByte, null)
+        };
+    }
+
+    private struct ColorSet
+    {
+        public ConsoleColor Background;
+        public ConsoleColor Foreground;
     }
 }
--- Natrium.Devices/Screen.cs
+++ Natrium.Devices/Screen.cs
@@ -1,11 +1,14 @@
 namespace Natrium.Devices
 {
-// 0 -> Index (W)
-// 1 -> Value (W)
-// 2 -> Clear (W)
-// 3 -> AutoIncrement (W)
-// 4 -> Width (R)
-// 5 -> Height (R)
+    // 0 -> Index (W)
+    // 1 -> Value (W)
+    // 2 -> Color (W)
+    // 3 -> Clear (W)
+    // 4 -> AutoIncrement (W)
+    // 5 -> Width (R)
+    // 6 -> Height (R)
+    // Colors: 0: Default, 1: Red, 2: Green, 3: Blue, 4: Cyan, 5:Magenta, 6: Yellow
+    //         A: Black, B: White, C: Gray, D: DarkGray
     public class Screen : IDevice
     {
         private uint _nextIndex;
@@ -13,11 +16,13 @@ namespace Natrium.Devices
         public Screen(int width, int height)
         {
             Data = new char[width * height];
+            Color = new byte[width * height];
             Width = width;
             Height = height;
         }
 
         public char[] Data { get; }
+        public byte[] Color { get; }
         public int Width { get; private set; }
         public int Height { get; private set; }
 
@@ -34,11 +39,11 @@ namespace Natrium.Devices
                     value = Data[_nextIndex];
                     break;
 
-                case 4:
+                case 5:
                     value = Width;
                     break;
 
-                case 5:
+                case 6:
                     value = Height;
                     break;
 
@@ -64,14 +69,27 @@ namespace Natrium.Devices
                         return false;
                     Data[_nextIndex] = (char)value;
                     if (_autoIncrement)
+                    {
+                        // if auto increment, apply color to next cell.
+                        if (_nextIndex + 1 < Color.Length)
+                            Color[_nextIndex + 1] = Color[_nextIndex];
                         _nextIndex++;
-                    break;
+                    }
 
-                case 2:
-                    System.Array.Fill(Data, ' ');
                     break;
                 
-                case 3 :
+                case 2:
+                    if (_nextIndex >= Color.Length)
+                        return false;
+                    Color[_nextIndex] = (byte)value;
+                    break;
+
+                case 3:
+                    System.Array.Fill(Data, ' ');
+                    System.Array.Fill(Color, (byte)0);
+                    break;
+                
+                case 4 :
                     _autoIncrement = value > 0;
                     break;
 
